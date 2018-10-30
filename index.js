8699da5266ffab09a8e989844a9587ab46523888
Willem Olding <willemolding@gmail.com>
2018-10-30 15:24:23 +1100

    adds working config.json and adds ability to call different apps

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const Container = require('./holochain-nodejs/lib/container');
 
 const server = express();
 server.use(bodyParser.json());
-const port = 3000;
+
+let installedHapps = {};
+
+/*======================================
+=            HTTP Callbacks            =
+======================================*/
 
 server.post('/call/', (req, res) => {
 	const {happ, zome, capability, func, data} = req.body;
@@ -14,14 +19,52 @@ server.post('/call/', (req, res) => {
 	console.log("Calling: ", {happ, zome, capability, func});
 	console.log("With: ", data);
 
-	const result = app.call(zome, capability, func, JSON.stringify(data));
-	console.log("Result: ", result);
-	res.json(result);
+	try {
+		const result = installedHapps[happ].call(zome, capability, func, JSON.stringify(data));
+		console.log("Result: ", result);
+		res.json(result);		
+	} catch (err) {
+		console.error(err);
+		res.status(400).send("Could not call holochain function");
+	}
+
 });
 
-// start the holochain app
-const app = Container.loadAndInstantiate("./holochain-nodejs/app-spec-rust.hcpkg");
-app.start();
+/*=====  End of HTTP Callbacks  ======*/
+
+
+
+
+const configPath = process.argv[2];
+let config;
+
+if(configPath) {
+	try {
+		config = require(configPath);
+	} catch (err) {
+		console.error("Error:", configPath, "does not exist");
+		process.exit()
+	}
+} else {
+	console.error("Error: Script requires a single argument which is the path to a config json file");
+	process.exit();
+}
+
+
+const {port, happs} = config;
+
+// load and start all the apps
+Object.keys(happs).forEach((appName) => {
+	try {
+		installedHapps[appName] = Container.loadAndInstantiate(happs[appName]);
+		installedHapps[appName].start();
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 // start the web server
 server.listen(port, () => console.log(`Holochain HTTP server listening on port ${port}!`));
+
+
+
